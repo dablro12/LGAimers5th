87ffac47624ad3b8abfb75c209ee66407a1748d3
dablro12 <dablro1232@gmail.com>
2025-02-03 17:40:29 +0900

    [ADD] Baseline

---
+++ utils/public.py
@@ -0,0 +1,23 @@
+#%% [Default] Data Analysis 
+import pandas as pd
+import numpy as np
+
+
+#%% [Visualization] Data Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+#%% [Method] Machine Learning  
+from sklearn.preprocessing import  OrdinalEncoder
+from sklearn.ensemble import ExtraTreesClassifier
+
+#%% [Method] Deep Learning
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+#%% [Metric] Evaluation
+from sklearn.metrics import roc_auc_score
